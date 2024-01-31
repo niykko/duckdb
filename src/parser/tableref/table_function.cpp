a78f2445a5fc7054d08302d5346f22bd6ed5ab0f
niykko <nico.faden@student.uni-tuebingen.de>
2024-01-31 14:15:22 +0100

    Updated TableFunctionRef functions

--- src/parser/tableref/table_function.cpp
+++ src/parser/tableref/table_function.cpp
@@ -21,6 +21,9 @@ bool TableFunctionRef::Equals(const TableRef &other_p) const {
 		return false;
 	}
 	auto &other = other_p.Cast<TableFunctionRef>();
+	if (with_ordinality != other.with_ordinality) {
+		return false;
+	}
 	return function->Equals(*other.function);
 }
 
@@ -29,6 +32,7 @@ unique_ptr<TableRef> TableFunctionRef::Copy() {
 
 	copy->function = function->Copy();
 	copy->column_name_alias = column_name_alias;
+	copy->with_ordinality = with_ordinality;
 	CopyProperties(*copy);
 
 	return std::move(copy);
